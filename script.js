4df43b3b9d2e77f5087eb3e48247d76011223dda
Jesse Harlan <jesse.harlan@centralia.edu>
2023-04-25 09:07:59 -0700

    added base function to edit

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Edit the following code to have the following behavior:
+    // When the input has a number and the add button is pressed the function fires and should do the following:
+        // Adds the number to the running total 
+            // if this doesnt behave the way you expect then try looking up how to convert a string to a number, inputs are treated as strings by default
+        // Updates the h3 element (id="display") to the following "Total: <the value of total>";
+        // Clears the input so the user can add a new value.
+
 let total = 0;
-let display = document.getElementById("total");
-let input = document.getElementById("number");
 
 function add() {
-    total = total + Number(input.value);
-    display.innerHTML = "Total: " + total;
-    input.value = "";
+    
 }
